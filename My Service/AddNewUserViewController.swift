5c637dcb668a8334f044c32087f8409de9f16135
apiwat wattanasura <apiwatwattanasura@apiwats-MacBook-Pro.local>
2017-06-15 16:30:22 +0700

    zxx1

---
+++ My Service/AddNewUserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddNewUserViewController.swift
+//  My Service
+//
+//  Created by apiwat wattanasura on 6/15/17.
+//  Copyright © 2017 SNRU. All rights reserved.
+//
+
+import UIKit
+
+class AddNewUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("This is AddNewUser")
+
+        // Do any additional setup after loading the view.
+    } // Main Method
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    } // didReceive
+    
+    
+   
+} //Main Class
--- My Service/ViewController.swift
+++ My Service/ViewController.swift
@@ -12,8 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+       
+        //Process connected Http
+        let urlPHP = "http://androidthai.in.th"
+        //Change String to url String
+        let myURL = URL(string: urlPHP)
+        
+        let request = NSMutableURLRequest (url: myURL!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            
+            
+            data, response, error in
+            if error != nil{
+                print("Error ==> \(error)")
+            } else{
+                
+                if let unwrappedData = data {
+                    
+                    
+                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    let strJSON = dataString as Any
+                    print("strJSON ==> \(strJSON)")
+                    
+                    
+                }
+            }// if1
+            
+            
+            
+        }
+        
+        
+        
+        task.resume()
+        
+        
+        
+        
+        
+    } // Main Metthod
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
