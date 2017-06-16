4a5e98dd1cf14920243dc72bc61efbf38003cd61
apiwat wattanasura <apiwatwattanasura@apiwats-MacBook-Pro.local>
2017-06-16 14:19:30 +0700

    textfield Ok

--- My Service/ViewController.swift
+++ My Service/ViewController.swift
@@ -12,44 +12,72 @@ class ViewController: UIViewController {
     
     //Explicit
     var myString = ""
+    var strUser: String? = nil
+    var strPassword: String? = nil
+    
+    //Check Space
+    
+    
+    
+    @IBOutlet weak var userTextFieid: UITextField!
+    
+    @IBOutlet weak var passwordTextFieid: UITextField!
+    
+    
+    @IBOutlet weak var alertLabel: UILabel!
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+        
+        //Get Value From TextFieid
+        strUser = userTextFieid.text
+        strPassword = passwordTextFieid.text
+        
+        if strUser?.characters.count == 0 || strPassword?.characters.count == 0 {
+            alertLabel.alpha = 1
+        }else{
+            
+            alertLabel.alpha = 0
+            
+        }// if
+        
+    } //loginButton
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        //Process connected Http
-        let urlPHP = "http://androidthai.in.th"
-        //Change String to url String
-        let myURL = URL(string: urlPHP)
+//        //Process connected Http
+//        let urlPHP = "http://androidthai.in.th"
+//        //Change String to url String
+//        let myURL = URL(string: urlPHP)
+//        
+//        let request = NSMutableURLRequest (url: myURL!)
+//        let task = URLSession.shared.dataTask(with: request as URLRequest){
+//            
+//            
+//            data, response, error in
+//            if error != nil{
+//                print("Error ==> ")
+//            } else{
+//                
+//                if let unwrappedData = data {
+//                    
+//                    
+//                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+//                    let strJSON = dataString as Any
+//                    print("strJSON ==> \(strJSON)")
+//                    
+//                    
+//                }
+//            }// if1
+//                }
+//       task.resume()
         
-        let request = NSMutableURLRequest (url: myURL!)
-        let task = URLSession.shared.dataTask(with: request as URLRequest){
+        func readJSON(strURL: String) -> Void{
             
-            
-            data, response, error in
-            if error != nil{
-                print("Error ==> ")
-            } else{
-                
-                if let unwrappedData = data {
-                    
-                    
-                    let dataString = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                    let strJSON = dataString as Any
-                    print("strJSON ==> \(strJSON)")
-                    
-                    
-                }
-            }// if1
-            
-            
-            
-        }
-        
-        
-        
-        task.resume()
-        
-        
+        } //readJSON
         
         
         
